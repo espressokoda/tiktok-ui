001a387194ae8d23b5de7d2934cb45754e3a17fc
nht <harry.nht2@gmail.com>
2022-07-22 14:13:22 +0700

    build header ui part 2

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/ddcb57a7bcd8bf0fc01c18338b2caf59~c5_300x300.webp?x-expires=1658635200&x-signature=W9%2FjJO8B2rcMtMLns2b%2FFbz9yIw%3D"
+                alt="Hoaa"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle}></FontAwesomeIcon>
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
